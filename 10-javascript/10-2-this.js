aad89deb663e713c5318539054b4277b4c6a0756
louis-25 <20151577@vision.hoseo.edu>
2021-02-24 22:22:37 +0900

    config

---
+++ 10-javascript/10-2-this.js
@@ -0,0 +1,17 @@
+console.log(this);
+
+function simpleFunc() {
+    console.log(this);
+}
+simpleFunc();
+
+class Counter {
+    count = 0;
+    increase = function() {
+        console.log(this);
+    };
+}
+const counter = new Counter();
+counter.increase();
+const caller = counter.increase;
+caller();
---
+++ 10-javascript/10-3-module2.js
@@ -0,0 +1,3 @@
+import run, {print as printMessage} from './10-3-module.js';
+console.log(run(1, 2));
+printMessage();
