a055788385bf5b82460e78bbc6b86fb1265855a7
louis-25 <20151577@vision.hoseo.edu>
2021-02-25 10:21:25 +0900

    project setup

--- 10-javascript/10-3-module.js
+++
@@ -1,7 +0,0 @@
-export default function add(a, b) {
-    return a+b;
-}
-
-export function print() {
-    console.log('print');
-}
--- 10-javascript/10-3-module2.js
+++
@@ -1,3 +0,0 @@
-import run, {print as printMessage} from './10-3-module.js';
-console.log(run(1, 2));
-printMessage();
